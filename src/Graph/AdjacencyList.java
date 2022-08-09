d8be0252f97183a9dbf2ea7c37921ac19c037439
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2022-08-09 17:41:25 +0530

    second commit

--- src/Graph/AdjacencyList.java
+++ src/Graph/AdjacencyList.java
@@ -1,4 +1,57 @@
 package Graph;
 
+import java.util.*;
+
 public class AdjacencyList {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        adjacencyList(sc,sc.nextInt(),sc.nextInt());
+//    }
+//    public static void adjacencyList(Scanner sc,int m,int n){
+//        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+//        for (int i = 0; i < m; i++) {
+//            adj.add(new ArrayList<>());
+//
+//        }
+//        int u,v;
+//        for (int i = 0; i < m; i++) {
+//            u=sc.nextInt();
+//            v=sc.nextInt();
+//            adj.get(u).add(v);
+//            adj.get(v).add(u);
+//        }
+//        System.out.println(adj);
+//    }
+
+
+    Map<Integer, LinkedList<Integer>> obj;
+    public AdjacencyList(){
+        obj= new HashMap<>();
+    }
+    public void addEdge(int v1,int v2,boolean isbidirectional){
+        LinkedList<Integer> v1neighbour = obj.getOrDefault(v1,new LinkedList<>());
+        v1neighbour.add(v2);
+        obj.put(v1,v1neighbour);
+        if(isbidirectional){
+            LinkedList<Integer> v2neighbour =obj.getOrDefault(v2,new LinkedList<>());
+            v2neighbour.add(v1);
+            obj.put(v2,v2neighbour);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        AdjacencyList g = new AdjacencyList();
+        g.addEdge(0,1,true);
+        g.addEdge(0,3,true);
+        g.addEdge(0,8,true);
+        g.addEdge(1,7,true);
+        g.addEdge(2,3,true);
+        g.addEdge(2,5,true);
+        g.addEdge(2,7,true);
+        g.addEdge(3,4,true);
+        g.addEdge(4,8,true);
+        g.addEdge(5,6,true);
+    }
 }
--- src/Graph/BFS.java
+++ src/Graph/BFS.java
@@ -1,4 +1,28 @@
 package Graph;
 
+import java.util.*;
+
 public class BFS {
+    Map<Integer , LinkedList<Integer>> obj;
+    public BFS(){
+        obj = new HashMap<>();
+    }
+    public void bfsTraversal(int source){
+        Queue<Integer> bfs = new LinkedList<>();
+        bfs.add(source);
+        Set<Integer> vis = new HashSet<>();
+        vis.add(source);
+        while(!bfs.isEmpty()){
+            int front = bfs.poll();
+            System.out.println(front);
+            LinkedList<Integer> neighbourList = obj.get(front);
+            for (int res:neighbourList) {
+                if(!vis.contains(res)){
+                    bfs.add(res);
+                    vis.add(res);
+                }
+            }
+        }
+    }
+
 }
