dec797da32415d467938419bbb856e03deae02a3
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2022-09-05 23:28:10 +0530

    Adding files

---
+++ src/DynamicProgramming/subsetSum.java
@@ -0,0 +1,29 @@
+package DynamicProgramming;
+
+public class subsetSum {
+    public static void main(String[] args) {
+        int[] arr = {2,3,7,8,10};
+        int sum = 31;
+        int n = arr.length;
+        System.out.println(subsetSum(arr,sum,n));
+    }
+
+    public static boolean subsetSum(int[] arr,int sum,int n){
+        boolean[][] dp = new boolean[n+1][sum+1];
+
+        for (int i = 0; i < n + 1 ; i++) {
+            dp[i][0]=true;
+        }
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < sum+1; j++) {
+                if(arr[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+}
